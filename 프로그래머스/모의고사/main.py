e8dea11671c8fb1fb5f4e717d05fbb028c71b483
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-06-27 15:56:36 +0900

    프로그래머스
    Level1 모의고사 풀이

---
+++ 프로그래머스/모의고사/main.py
@@ -0,0 +1,20 @@
+def solution(answers):
+    answer=[]
+    fakeAnswer1 = [1, 2, 3, 4, 5]
+    fakeAnswer2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    fakeAnswer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = {0, 0, 0}
+
+    for i in range(len(answers)):
+        if(answers[i] == fakeAnswer1[i%5]):
+            score[0] += 1
+        if(answers[i] == fakeAnswer2[i%8]):
+            score[1] += 1
+        if(answers[i] == fakeAnswer3[i%10]):
+            score[2] += 1
+
+    winner = max(score)
+
+    for i in range(len(score)):
+        if score[i] == winner:
+            answer.append(i+1)
