bbf02d0a49039c704ce580d63b1771d7d5ed3716
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-07-06 18:00:19 +0900

    소수 만들기

---
+++ 프로그래머스/Level 1/소수 만들기/main.py
@@ -0,0 +1,18 @@
+from itertools import combinations   
+
+def solution(nums):
+    answer = 0
+    cmb = list(combinations(nums,3))
+    for i in cmb:
+        if prime_number(sum(i))==True:
+            answer+=1
+    return answer
+            
+            
+def prime_number(number):
+    for k in range(2, number):
+        if number % k == 0:
+            return False
+    return True
+
+print(solution([1,2,3,4]))
