148e02f00f400bb4ab2eae9febcf157d96a9d048
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-07-07 12:52:04 +0900

    음양 더하기

---
+++ 프로그래머스/Level 1/음양 더하기/main.py
@@ -0,0 +1,11 @@
+def solution(absolutes, signs):
+    answer = 123456789
+    sum=0
+    for i in range(len(absolutes)):
+        if signs[i] == True: sum+=absolutes[i]
+        else: sum-=absolutes[i]
+    print(sum)
+
+    return answer
+
+solution([4,7,12], [True,False,True])
