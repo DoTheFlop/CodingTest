3da7a77f4c0d63c3be2ad3939343ade3d28afaef
CYC-ChoiYoungChang <94531328+DoTheFlop@users.noreply.github.com>
2022-09-20 14:57:32 +0900

    점프와 순간 이동

---
+++ 프로그래머스/Level 2/점프와 순간 이동/main.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    while(n > 0):
+        answer += n % 2
+        n //= 2
+    return answer
+
+def solution2(n):
+    return bin(n).count('1')
+    
+solution2(6)
