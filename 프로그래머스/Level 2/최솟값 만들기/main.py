5a5ddff68e35f608adb24a2fc99e70b4e3e3dd16
CYC-ChoiYoungChang <94531328+DoTheFlop@users.noreply.github.com>
2022-09-20 15:58:21 +0900

    최솟값 만들기

---
+++ 프로그래머스/Level 2/최솟값 만들기/main.py
@@ -0,0 +1,11 @@
+def solution(A,B):
+    answer = 0
+    A.sort(), B.sort(reverse = True)
+    for i in range(len(A)):
+        answer += A[i] * B[i]
+    return answer
+
+def solution2(A,B):
+    return sum(a * b for a, b in zip(sorted(A), sorted(B, reverse = True)))
+
+print(solution2([1, 4, 2], [5, 4, 4]))
