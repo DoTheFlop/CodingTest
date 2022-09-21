5db48bafd86e84457be198305b25ab8765e08078
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-09-21 18:29:29 +0900

    행렬의 곱셈

--- 프로그래머스/Level 2/행렬의 곱셈/main.py
+++ 프로그래머스/Level 2/행렬의 곱셈/main.py
@@ -1,9 +1,11 @@
 import numpy as np
 
 def solution(arr1, arr2):
-    np.array(arr1)
-    np.array(arr2)
+    arr1 = np.array(arr1)
+    arr2 = np.array(arr2)
     answer = arr1 @ arr2
-    return answer
+    return answer.tolist()
 
 print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
+
+#numpy 안쓰고 다시 풀기
