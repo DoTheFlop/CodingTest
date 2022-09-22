c1eb9ffa759b60e48dcaac274e671f6b35629a75
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-09-22 12:42:46 +0900

    [1차] 캐시

---
+++ 프로그래머스/Level 2/[1차] 캐시/main.py
@@ -0,0 +1,24 @@
+def solution(cacheSize, cities):
+    cities = [i.lower() for i in cities]
+    queue = []
+    cacheHit=0
+    cacheMiss=0
+    for i in cities:
+        if(i in queue):
+            idx = queue.index(i)
+            queue.pop( idx )
+            queue.insert(0, i)
+            cacheHit += 1
+            cacheMiss -= 5
+        else:
+            queue.insert(0, i)
+            cacheMiss += 5
+            
+        if(len(queue) > cacheSize):
+            queue.pop()
+    answer = cacheHit + cacheMiss
+    return answer
+
+solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"])
+
+solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"])
