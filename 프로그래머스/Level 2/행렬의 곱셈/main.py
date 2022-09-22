b71541d12a91783466528baf056682175a978b65
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-09-22 12:22:43 +0900

    행렬의 곱셈

--- 프로그래머스/Level 2/행렬의 곱셈/main.py
+++ 프로그래머스/Level 2/행렬의 곱셈/main.py
@@ -9,3 +9,13 @@ def solution(arr1, arr2):
 print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
 
 #numpy 안쓰고 다시 풀기
+
+def solution2(arr1, arr2):
+    answer = [[0]*len(arr2[0]) for _ in range(len(arr1))]
+    
+    for i in range(len(arr1)): 
+        lists = []
+        for j in range(len(arr2[0])): 
+            for k in range(len(arr1[0])): 
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+    return answer
