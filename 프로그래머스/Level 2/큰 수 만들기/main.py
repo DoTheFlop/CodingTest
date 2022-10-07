54a8eee786e60d37fe07349e4d4e5bc94c2f06a1
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-07 17:38:40 +0900

    큰 수 만들기

---
+++ 프로그래머스/Level 2/큰 수 만들기/main.py
@@ -0,0 +1,17 @@
+def solution(number, k):
+    answer = []
+    
+    for i in number:
+        if not answer:
+            answer.append(i)
+            continue
+        while answer[-1] <= i and k > 0:
+            answer.pop()
+            k -= 1
+            if not answer or k <= 0:
+                break
+        answer.append(i)
+                
+    return answer
+    
+print(solution("4177252841", 4))
