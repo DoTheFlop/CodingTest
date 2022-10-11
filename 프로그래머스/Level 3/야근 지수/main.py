1df653f052b96a3b4ad6d8b4a2c9655c0af7f98c
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-11 15:18:21 +0900

    야근 지수

---
+++ 프로그래머스/Level 3/야근 지수/main.py
@@ -0,0 +1,23 @@
+import heapq
+
+def solution(n, works):
+    answer = 0
+    max_works = []
+    for i in works:
+        heapq.heappush(max_works, (-i, i))
+    for i in range(n):
+        if not max_works:
+            break
+        temp = heapq.heappop(max_works)
+        temp = ( -(temp[1] - 1), temp[1] - 1 )
+        if temp[1] < 1:
+            continue
+        else:
+            heapq.heappush(max_works, temp)
+            
+    while max_works:
+        a = heapq.heappop(max_works)
+        answer += a[1]*a[1]
+    return answer
+
+print(solution(4, [4, 3, 3]))
