1e6768b45130f79b2ec6aa86356e91db4d7248e6
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-23 20:18:57 +0900

    내림차 순으로 변경

--- 프로그래머스/Level 2/H-index/main.py
+++ 프로그래머스/Level 2/H-index/main.py
@@ -1,8 +1,8 @@
 def solution(citations):
-    citations.sort()
-    for idx , citation in enumerate(citations):
-        if citation >= len(citations) - idx :
-            return len(citations) - idx
-    return 0
+    citations.sort(reverse=True)
+    for idx, citation in enumerate(citations):
+        if citation <= idx :
+            return idx
+    return len(citations)
 
 print(solution([3, 0, 6, 1, 5]))
