2c547c8c0f6c36669189f92885488a2ba9175247
CYC-ChoiYoungChang <choiyc1446@gmail.com>
2022-10-30 20:26:23 +0900

    .

--- 프로그래머스/Level 1/모의고사/main.py
+++ 프로그래머스/Level 1/모의고사/main.py
@@ -3,7 +3,7 @@ def solution(answers):
     fakeAnswer1 = [1, 2, 3, 4, 5]
     fakeAnswer2 = [2, 1, 2, 3, 2, 4, 2, 5]
     fakeAnswer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    score = {0, 0, 0}
+    score = [0, 0, 0]
 
     for i in range(len(answers)):
         if(answers[i] == fakeAnswer1[i%5]):
@@ -18,3 +18,6 @@ def solution(answers):
     for i in range(len(score)):
         if score[i] == winner:
             answer.append(i+1)
+    return answer
+
+print(solution([1,2,3,4,5]))
--- 프로그래머스/Level 2/ 더 맵게/main.py
+++ 프로그래머스/Level 2/ 더 맵게/main.py
@@ -8,16 +8,14 @@ def solution(scoville, K):
         if( low >= K ):
             heapq.heappush(scoville, low)
             break
-        else:
-            if len(scoville) == 0:
-                answer = -1
-                break
+        elif len(scoville) == 0:
+            answer = -1
+            break
         high = heapq.heappop(scoville)
         heapq.heappush(scoville, low + (high * 2))
         answer += 1
         
     return answer
-
 
 
 print(solution([1, 2, 3, 9, 10, 12], 7))
